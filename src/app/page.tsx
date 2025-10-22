632049e9937f04aabecac06f801939b5a569939a
Raihan OiOi <rayaindonesia599@gmail.com>
2025-10-23 05:25:53 +0700

    Second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="">
+                <div>
+                    <h1 className="text-white text-2xl">Hello World!</h1>
+                </div>
+            </main>
+        </div>
+    );
+}
